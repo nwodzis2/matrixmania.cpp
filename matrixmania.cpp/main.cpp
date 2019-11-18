8ece92d56b27b53073c6b931bcfdfe9dd70422d4
Nathan Wodzisz <nwodzis2@kent.edu>
2019-11-17 20:46:37 -0500

    Initial Commit

---
+++ matrixmania.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  matrixmania.cpp
+//
+//  Created by Nathan wodzisz on 11/17/19.
+//  Copyright © 2019 Nathan wodzisz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
